a2f0c2de79a33228459824a49d23146416df3cc7
Jackie <jaclynmeyerstudio@gmail.com>
2021-06-10 21:39:02 -0400

    first solution TOP JS #!

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
-
-// math.random? .toLowercase
-
-
-// write a function compuerPlay(); that will randomly return 'Rock' 'Paper' or 'Scissors'.  Log in console  
 function computerPlay(arr){
-  for(let i = 0; i < 2; i++){
-    randomItem = Math.floor(Math.random() * game.length);
-    const item = arr[randomItem]
-    return item;
+    const randomItem = Math.floor(Math.random() * game.length);
+    const item = arr[randomItem];
+    return item;  
+}
+
+const game = ["rock", "paper", "scissors"];
+const computerSelection = computerPlay(game);
+
+let playerSelection = prompt("Rock, Paper or Scissors?");
+
+function playRound(playerSelection, computerSelection){
+    const playerWin = (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") || (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") || (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper")
+    const playerLose = (computerSelection == "scissors" && playerSelection.toLowerCase() == "paper") || (computerSelection == "paper" && playerSelection.toLowerCase() == "rock") || (computerSelection == "rock" && playerSelection.toLowerCase() == "scissors");
+    console.log(playerSelection, computerSelection);
+    console.log(playerWin, playerLose);
+    if(playerSelection.tolowerCase == computerSelection){
+        return "you tied! Try again!"
+    }
+
+   else  if (playerWin){
+         return `You Win! ${computerSelection} beats ${playerSelection}`;
+     }
+
+    else if (playerLose){
+        return `You Lose! ${playerSelection} beats ${computerSelection}`;
+     }    
+
+   else {
+       return "Please type rock, paper or scissors";
    }
 }
 
-const game = ["Rock", "Paper", "Scissors"];
-const result = computerPlay(game);
-console.log(result);
+console.log(playRound(playerSelection, computerSelection));
+console.log(playerSelection, computerSelection);
+
 
 
